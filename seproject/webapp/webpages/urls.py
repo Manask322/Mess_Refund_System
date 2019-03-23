cac58bfdf7923a608564685aab65d9939efa0c2e
Manask322 <16it226.manas@nitk.edu.in>
2019-03-23 15:16:33 +0530

    resolved error

--- seproject/webapp/webpages/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url, include
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.HomePageView.as_view(), name="homepage"),
-    url(r'^terms$', views.TermsView.as_view(), name="terms"),
-]
